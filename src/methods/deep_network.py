ce60a634953d9b8e71b59be4cc8cb0f23a2fae05
Yshai <yshai.dinee-baumgarten@epfl.ch>
2024-05-19 20:17:00 +0200

    transformer

--- src/methods/deep_network.py
+++ src/methods/deep_network.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
+import numpy as np
+
+from src.utils import accuracy_fn, onehot_to_label
+
 
 ## MS2
 
@@ -71,12 +75,12 @@ class CNN(nn.Module):
         super().__init__()
 
         # Convolutional layers
-        self.conv2d1 = nn.Conv2d(n, filters[0], kernel_size=3, padding=1) 
+        self.conv2d1 = nn.Conv2d(input_channels, filters[0], kernel_size=3, padding=1)
         self.conv2d2 = nn.Conv2d(filters[0], filters[1], kernel_size=3, padding=1) 
         self.conv2d3 = nn.Conv2d(filters[1], filters[2], kernel_size=3, padding=1)
 
         # Fully connected layers
-        self.fc1 = nn.Linear(filters[2]*3*3, size[0]) 
+        self.fc1 = nn.Linear(filters[2]*3*3, sizes[0])
         self.fc2 = nn.Linear(sizes[0], sizes[1])
         self.fc3 = nn.Linear(sizes[1], n_classes)
 
@@ -106,6 +110,96 @@ class CNN(nn.Module):
         return preds
 
 
+def patchify(images, n_patches):
+    n, c, h, w = images.shape
+
+    assert h == w  # We assume square image.
+
+    patches = torch.zeros(n, n_patches ** 2, h * w * c // n_patches ** 2)
+    patch_size = h // n_patches
+
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+                # Extract the patch of the image.
+                patch = image[:, i * patch_size: (i + 1) * patch_size, j * patch_size: (j + 1) * patch_size]
+
+                # Flatten the patch and store it.
+                patches[idx, i * n_patches + j] = patch.flatten()
+
+    return patches
+
+
+def get_positional_embeddings(sequence_length, d):
+    result = torch.ones(sequence_length, d)
+    for i in range(sequence_length):
+        for j in range(d // 2):
+            result[i, 2 * j] = np.sin(i / (10000 ** ((2 * j) / d)))
+            result[i, 2 * j + 1] = np.cos(i / (10000 ** ((2 * j) / d)))
+    return result
+
+
+class MyMSA(nn.Module):
+    def __init__(self, d, n_heads=2):
+        super(MyMSA, self).__init__()
+        self.d = d
+        self.n_heads = n_heads
+
+        assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
+        d_head = int(d / n_heads)
+        self.d_head = d_head
+
+        self.q_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.k_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.v_mappings = nn.ModuleList([nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, sequences):
+        result = []
+        for sequence in sequences:
+            seq_result = []
+            for head in range(self.n_heads):
+                # Select the mapping associated to the given head.
+                q_mapping = self.q_mappings[head]
+                k_mapping = self.k_mappings[head]
+                v_mapping = self.v_mappings[head]
+
+                seq = sequence[:, head * self.d_head: (head + 1) * self.d_head]
+
+                # Map seq to q, k, v.
+                q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+
+                attention = (q @ k.T) / np.sqrt(self.n_heads)
+                attention = self.softmax(attention)
+
+                seq_result.append(attention @ v)
+            result.append(torch.hstack(seq_result))
+        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+
+class MyViTBlock(nn.Module):
+    def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+        super(MyViTBlock, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        self.norm1 = nn.LayerNorm(hidden_d)
+        self.mhsa = MyMSA(hidden_d, n_heads)
+        self.norm2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+        )
+
+    def forward(self, x):
+         # MHSA + residual connection.
+        out = x + self.mhsa(self.norm1(x))
+        # Feedforward + residual connection
+        out = out + self.mlp(self.norm2(out))
+        return out
+
+
 class MyViT(nn.Module):
     """
     A Transformer-based neural network
@@ -117,11 +211,36 @@ class MyViT(nn.Module):
         
         """
         super().__init__()
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+
+        self.chw = chw  # (C, H, W)
+        self.n_patches = n_patches
+        self.n_blocks = n_blocks
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+
+        # Input and patches sizes
+        assert chw[1] % n_patches == 0  # Input shape must be divisible by number of patches
+        assert chw[2] % n_patches == 0
+        self.patch_size = (chw[1] // n_patches, chw[2] // n_patches)
+
+        # Linear mapper
+        self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+
+        # Learnable classification token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+
+        # Positional embedding
+        self.positional_embeddings = get_positional_embeddings(n_patches ** 2 + 1, hidden_d)
+
+        # Transformer blocks
+        self.blocks = nn.ModuleList([MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
+
+        # Classification MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
 
     def forward(self, x):
         """
@@ -133,11 +252,31 @@ class MyViT(nn.Module):
             preds (tensor): logits of predictions of shape (N, C)
                 Reminder: logits are value pre-softmax.
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        n, c, h, w = x.shape
+
+        # Divide images into patches.
+        patches = patchify(x, self.n_patches)
+
+        # Map the vector corresponding to each patch to the hidden size dimension.
+        tokens = self.linear_mapper(patches)
+
+        # Add classification token to the tokens.
+        tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim=1)
+
+        # Add positional embedding.
+        preds = tokens
+        preds += self.positional_embeddings.repeat(n, 1, 1)
+
+        # Transformer Blocks
+        for block in self.blocks:
+            preds = block(preds)
+
+        # Get the classification token only.
+        preds = preds[:, 0]
+
+        # Map to the output distribution.
+        preds = self.mlp(preds)
+
         return preds
 
 
@@ -197,19 +336,19 @@ class Trainer(object):
             x, y = batch
 
             # Run forward pass.
-            logits = model(x)
+            logits = self.model(x)
             
             # Compute loss.
-            loss = criterion(logits, y)
+            loss = self.criterion(logits, y)
             
             # Run backward pass.
             loss.backward()
             
             # Update the weights using 'optimizer'.
-            optimizer.step()
+            self.optimizer.step()
             
             # Zero-out the accumulated gradients.
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
 
             print('\rEp {}/{}, it {}/{}: loss train: {:.2f}, accuracy train: {:.2f}'.
                   format(ep + 1, self.epochs, it + 1, len(dataloader), loss,
@@ -240,7 +379,7 @@ class Trainer(object):
                 # Get batch of data.
                 x, y = batch
                 curr_bs = x.shape[0]
-                pred = model(x)
+                pred = self.model(x)
                 pred_labels = torch.cat(pred_labels, onehot_to_label(pred))
                 acc_run += accuracy_fn(pred, y) * curr_bs
             acc = 100 * acc_run / len(dataloader.dataset)
--- src/methods/pca.py
+++ src/methods/pca.py
@@ -38,11 +38,25 @@ class PCA(object):
         Returns:
             exvar (float): explained variance of the kept dimensions (in percentage, i.e., in [0,100])
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        # Compute the mean of data
+        self.mean = np.mean(training_data, axis=0)
+        # Center the data with the mean
+        X_tilde = training_data - self.mean
+        # Create the covariance matrix
+        C = np.cov(X_tilde, rowvar=False)  # Ensure each row represents a variable
+        # Compute the eigenvectors and eigenvalues
+        eigvals, eigvecs = np.linalg.eigh(C)
+        # Sort eigenvalues and eigenvectors in descending order
+        sorted_indices = np.argsort(eigvals)[::-1]
+        eigvals = eigvals[sorted_indices]
+        eigvecs = eigvecs[:, sorted_indices]
+        # Choose the top d eigenvalues and corresponding eigenvectors
+        self.W = eigvecs[:, :self.d]
+        eg = eigvals[:self.d]
+
+        # Compute the explained variance
+        exvar = np.sum(eg) / np.sum(eigvals) * 100
+
         return exvar
 
     def reduce_dimension(self, data):
@@ -54,11 +68,11 @@ class PCA(object):
         Returns:
             data_reduced (array): reduced data of shape (N,d)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+        # Center the data with the mean
+        X_tilde = data - self.mean
+        # Project the data using W
+        data_reduced = X_tilde @ self.W
+
         return data_reduced
         
 
